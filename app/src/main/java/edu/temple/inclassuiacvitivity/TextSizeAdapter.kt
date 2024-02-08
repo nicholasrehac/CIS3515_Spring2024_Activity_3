d7a1ccc12eaf4a39b0dcf42090ce3917f0a95dce
Nicholas Rehac <tup88289@temple.edu>
2024-02-08 11:38:48 -0500

    added TextSizeAdapter constructor, implemented getDropDownView

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,35 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val context: Context, private val sizes: Array<Int>) : BaseAdapter() {
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return sizes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return sizes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        //
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var textView = TextView(context)
+        textView.text = sizes[position].toString()
+        textView.textSize = sizes[position].toFloat()
+        textView.setPadding(20,20,20,20)
+        return textView
     }
 
 }
